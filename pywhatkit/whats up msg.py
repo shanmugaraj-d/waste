cc3e45e1f1f08a05a1bd6e1081add6eb1b011872
monkey d luffy <88392078+monkey-d-luffy1@users.noreply.github.com>
2022-05-01 12:57:20 +0530

    Add files via upload

---
+++ pywhatkit/whats up msg.py
@@ -0,0 +1,11 @@
+import pywhatkit as kit
+
+# kit.info("python")
+
+#kit.playonyt("90s gamer")
+
+#kit.search("python")
+
+#kit.sendwhatmsg_instantly("+917094926070", "hello")
+
+kit.sendwhatmsg(f"+91{7094926070}", "hello", 21, 23)     #somethimes not working
